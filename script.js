272c68e7b1d400e4a4f006aff09cc39378749e6c
Gabriel <gabriel.p1997@yahoo.com>
2020-05-25 19:17:04 -0300

    Mudança no layout da página.
    Mudança no layout e paleta de cores da página,
    Criação da função bloco
    Mudanças na função items

--- script.js
+++ script.js
@@ -1,53 +1,65 @@
 $(document).ready(function () {
 
-    function opcoes(local, nome, ...options) {
+    function opcoes(propriedade, local, nome, ...options) {
         const conteiner_select = $('<span>').text(nome).addClass('d-flex flex-column')
         const select = $('<select>').addClass('custom-select').attr('id', nome)
         conteiner_select.append(select)
         $(local).append(conteiner_select)
-        $('#' + nome).append('<option>Selecione</option>').attr('selected', 'true')
-        $('#' + nome).attr('propriedade', nome)
+        $('#' + nome).attr('propriedade', propriedade)
         options.forEach(function (item) {
             $('#' + nome).append('<option>' + item + '</option>');
         })
     }
 
     //flex-conteiner
-    $('<div>').appendTo('#comandos').attr('id', 'flex-conteiner').addClass('font-weight-bold')
-    $('#flex-conteiner').append($('<span>').html('<h4>Flex-Conteiner</h4>'))
-    $('<div>').appendTo('#flex-conteiner').attr('id', 'corpo-flex-conteiner')
-
-    opcoes('#corpo-flex-conteiner', 'flex-direction', 'row', 'column', 'row-reverse', 'column-reverse')
-    opcoes('#corpo-flex-conteiner', 'flex-wrap', 'NoWrap', 'Wrap', 'Wrap-Reverse')
-    opcoes('#corpo-flex-conteiner', 'justify-content', 'flex-start', 'flex-end', 'space-around', 'space-between', 'center')
-    opcoes('#corpo-flex-conteiner', 'align-items', 'baseline', 'flex-start', 'flex-end', 'center', 'stretch')
-    opcoes('#corpo-flex-conteiner', 'align-content', 'flex-start', 'flex-end', 'center', 'space-around', 'space-between')
+    $('<div>').appendTo('#esquerda').attr('id', 'flex-conteiner').addClass('font-weight-bold d-flex flex-column')
+    $('#flex-conteiner').append($('<span>').html('<h5>Flex-Conteiner</h5>'))
+    $('<div>').appendTo('#flex-conteiner').attr('id', 'corpo-flex-conteiner').attr('data-id', 'flex-conteiner')
+    opcoes('flexDirection', '#corpo-flex-conteiner', 'flex-direction', 'row', 'column', 'row-reverse', 'column-reverse')
+    opcoes('flexWrap', '#corpo-flex-conteiner', 'flex-wrap', 'NoWrap', 'Wrap', 'Wrap-Reverse')
+    opcoes('justifyContent', '#corpo-flex-conteiner', 'justify-content', 'flex-start', 'flex-end', 'space-around', 'space-between', 'center')
+    opcoes('alignItems', '#corpo-flex-conteiner', 'align-items', 'stretch', 'flex-start', 'baseline', 'flex-end', 'center')
+    opcoes('alignContent', '#corpo-flex-conteiner', 'align-content', 'flex-start', 'flex-end', 'center', 'space-around', 'space-between')
 
     //flex-item
     function items(numero) {
-        $('<div>').appendTo('#comandos').addClass('font-weight-bold item-conteiner' + numero)
-        $('.item-conteiner' + numero).append($('<span>').html('<h4>item-conteiner</span>'))
-        $('<div>').appendTo('.item-conteiner' + numero).addClass('corpo-item-conteiner' + numero)
-
-        opcoes('.corpo-item-conteiner' + numero, 'align-self', 'auto', 'flex-start', 'flex-end', 'center', 'stretch')
-
-        const input = $('<span>').text('Flex-Grow').addClass('flex-grow')
+        $('<div>').appendTo('#comandos').addClass('flex-item font-weight-bold item-conteiner d-flex flex-column flex-wrap').attr('id', 'itemconteiner-' + numero)
+        $('#itemconteiner-' + numero).append($('<span>').html(`<h5>item-${numero}</span>`))
+        $('<div>').appendTo('#itemconteiner-' + numero).attr('id', 'corpo-item-conteiner' + numero).attr('data-id', 'item-' + numero)
+        opcoes('alignSelf', '#corpo-item-conteiner' + numero, 'align-self', 'auto', 'flex-start', 'flex-end', 'center', 'stretch')
+        const input = $('<span>').text('Flex-Grow:').addClass('flex-grow')
         input.append($('<input>').addClass('form-control').attr('type', 'number').attr('propriedade', 'flex-grow').val('0'))
-        $('.corpo-item-conteiner' + numero).append(input)
-        $('#align-self').attr('id', 'align-self' + numero) // substitui o id por um novo com numero diferente no fim.
+        $('#corpo-item-conteiner' + numero).append(input)
+        $('#align-self').attr('id', 'alignself-' + numero) // substitui o id por um novo com numero diferente no fim.
     }
+    let num = 1
+    if (num < 0) {
+        num = 1
+    }
+    let del = 2
+    if (del <= 1) {
+        del = 2
+    }
+    //blocos
+    function bloco(deletavel = false, altura = 70, largura = 70) {
+        if (deletavel) {
+            $('<div>').appendTo("#quadro").addClass("deletavel text-white d-flex position-relative").attr('id', 'deletavel-' + del)
+            $('#deletavel-' + del).html(`<button type="button" class="close" data-dismiss="alert" aria-label="Close"><h1>${num}</h1></button>`)
+        } else {
+            $('<div>').appendTo('#quadro').addClass('bloco').attr('id', 'bloco' + num).height(altura).width(largura)
+            $('#bloco' + num).addClass('d-flex justify-content-center align-items-center').html(`<h2 class="alert-heading">${num}</h2>`)
+        }
+        num++
+    }
+    bloco(true)
 
     items(1)
-    items(2)
-    items(3)
 
-    // retornar na saida o valor e id  do select
-    //selecionar o "pai" para monstrar a fonte das configuraçoes, ### ... [][][]
-    $('select').on('change', function (e) {
-        if (this.value !== 'Selecione')
-            $('#saida').html('<h3>#flex-conteiner { ' + $(this).attr('propriedade') + ': ' + $(this).val() + ';<br>' + '}</h3>');
-        console.log(e)
 
+    // retorna na saida o valor e id  do select
+    //selecionar o "pai" para monstrar a fonte das configuraçoes
+    $('select').on('change', function () {
+            $('#saida').html('<h2>#flex-conteiner { ' + $(this).attr('propriedade') + ': ' + $(this).val() + ';<br>' + '}</h2>');
+        console.log($(this).val())
     });
 })
-
